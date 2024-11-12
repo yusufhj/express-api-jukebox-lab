9fd2df10dfebe4688eae1baa2035c4f03a9b3c63
Yusuf AlHamad <ylive71@gmail.com>
2024-11-13 01:21:13 +0300

    DELETE

--- controllers/jukeboxes.js
+++ controllers/jukeboxes.js
@@ -5,8 +5,8 @@ const Jukebox = require('../models/jukebox.js');
 // Post /tracks
 router.post('/', async (req, res) => {
     try {
-      const newJukebox = await Jukebox.create(req.body);
-      res.status(201).json(newJukebox);
+      const newTrack = await Jukebox.create(req.body);
+      res.status(201).json(newTrack);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
@@ -25,12 +25,12 @@ router.get('/', async (req, res) => {
 // Get /tracks/:id
 router.get('/:trackId', async (req, res) => {
     try {
-        const foundJukebox = await Jukebox.findById(req.params.trackId);
-        if (!foundJukebox) {
+        const foundTrack = await Jukebox.findById(req.params.trackId);
+        if (!foundTrack) {
             res.status(404);
             throw new Error('Track not found');
         }
-        res.status(200).json(foundJukebox);
+        res.status(200).json(foundTrack);
     } catch (error) {
         if (res.statusCode === 404) {
             res.json({ error: error.message });
@@ -43,12 +43,12 @@ router.get('/:trackId', async (req, res) => {
 // Put /tracks/:id
 router.put('/:trackId', async (req, res) => {
     try {
-        const updatedJukebox = await Jukebox.findByIdAndUpdate(req.params.trackId, req.body, { new: true });
-        if (!updatedJukebox) {
+        const updatedTrack = await Jukebox.findByIdAndUpdate(req.params.trackId, req.body, { new: true });
+        if (!updatedTrack) {
             res.status(404);
             throw new Error('Track not found');
         }
-        res.status(200).json(updatedJukebox);
+        res.status(200).json(updatedTrack);
     } catch (error) {
         if (res.statusCode === 404) {
             res.json({ error: error.message });
@@ -58,5 +58,22 @@ router.put('/:trackId', async (req, res) => {
     }
 });
 
+// Delete /tracks/:id
+router.delete('/:trackId', async (req, res) => {
+    try {
+        const deletedTrack = await Jukebox.findByIdAndDelete(req.params.trackId);
+        if (!deletedTrack) {
+            res.status(404);
+            throw new Error('Track not found');
+        }
+        res.status(200).json(deletedTrack);
+    } catch (error) {
+        if (res.statusCode === 404) {
+            res.json({ error: error.message });
+        } else {
+            res.status(500).json({ error: error.message });
+        }
+    }
+});
 
 module.exports = router;
